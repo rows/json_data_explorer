d832dedeb41d28c74e6302341db67b87139c626b
Victor Botamedi <victor.botamedi@gmail.com>
2022-04-29 11:59:55 +0100

    Update test name

--- test/golden/multi_size_test.dart
+++ test/golden/multi_size_test.dart
@@ -9,17 +9,9 @@ import 'package:provider/provider.dart';
 import 'test_data.dart';
 
 void main() {
-  testGoldens('Weather types should look correct', (tester) async {
+  testGoldens('Tree should look correct on multiple device sizes',
+      (tester) async {
     final dynamic jsonObject = json.decode(nobelPrizesJson);
-
-    final widget = ChangeNotifierProvider(
-      create: (context) => DataExplorerStore()..buildNodes(jsonObject),
-      child: Consumer<DataExplorerStore>(
-        builder: (context, state, child) => JsonDataExplorer(
-          nodes: state.displayNodes,
-        ),
-      ),
-    );
 
     final builder = DeviceBuilder()
       ..overrideDevicesForAllScenarios(devices: [
@@ -68,34 +60,6 @@ void main() {
           ),
         ),
       );
-    // ..addScenario(
-    //     name: 'highlight entire node on hover',
-    //     widget: ChangeNotifierProvider(
-    //       create: (context) => DataExplorerStore()..buildNodes(jsonObject),
-    //       child: Consumer<DataExplorerStore>(
-    //         builder: (context, state, child) => JsonDataExplorer(
-    //           nodes: state.displayNodes,
-    //         ),
-    //       ),
-    //     ),
-    //     onCreate: (widgetKey) async {
-    //       final gesture = await tester.createGesture(
-    //         kind: PointerDeviceKind.mouse,
-    //       );
-    //       await gesture.addPointer(location: Offset.zero);
-    //       addTearDown(gesture.removePointer);
-    //       await tester.pump();
-    //
-    //       final finder = find.descendant(
-    //         of: find.byKey(widgetKey),
-    //         matching: find.text(
-    //           'laureates:',
-    //           findRichText: true,
-    //         ),
-    //       );
-    //       await gesture.moveTo(tester.getCenter(finder.first));
-    //       await tester.pumpAndSettle();
-    //     });
 
     await tester.pumpDeviceBuilder(builder);
     await screenMatchesGolden(tester, 'multi_size_test');
